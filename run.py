9c28979ee898043e688a27e50d8380bfc417cb45
Jean de Villiers <jeandevilliers@me.com>
2022-12-23 16:49:49 +0000

    Fix pep8 errors

--- run.py
+++ run.py
@@ -49,6 +49,6 @@ if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
-        debug=True  # Don't use in production. SET TO FALSE BEFORE SUBMITTING PROJECT FOR ASSESSMENT!!!
+        debug=True  # Don't use in production.
+                    # SET TO FALSE BEFORE SUBMITTING PROJECT FOR ASSESSMENT!!!
     )
-
